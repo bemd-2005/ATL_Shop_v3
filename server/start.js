c6255e7187db9cf71b33aec1cb3b12cd6f170dca
bemd-2005 <skinrafifa@gmail.com>
2026-06-21 18:40:45 +0100

    Fix: start.js utilise require() au lieu de execsync

--- server/start.js
+++ server/start.js
@@ -8,7 +8,8 @@ const dbPath = path.resolve(process.env.DB_PATH || './server/db/atl_shop.bd');
 // Vérifie si la base de données existe, sinon la crée
 if (!fs.existsSync(dbPath)) {
     console.log('Base de données non trouvée, initialisation...');
-    execSync(`node server/init_db.js`, { stdio: 'inherit' });
+    require('./db/init.js');
+    console.log('Initialisation terminée.');
 }else{
     console.log('Base de données trouvée, démarrage du serveur...');
 }
