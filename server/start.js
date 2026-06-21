285c2a54f14d61e08e99388cfadfa42740d71251
bemd-2005 <skinrafifa@gmail.com>
2026-06-21 19:44:24 +0100

    Fix: start.js utilise cwd explicite  pour  execsync

--- server/start.js
+++ server/start.js
@@ -8,9 +8,17 @@ const dbPath = path.resolve(process.env.DB_PATH || './server/db/atl_shop.bd');
 // Vérifie si la base de données existe, sinon la crée
 if (!fs.existsSync(dbPath)) {
     console.log('Base de données non trouvée, initialisation...');
-    require('./db/init.js');
-    console.log('Initialisation terminée.');
-}else{
+    try {
+        execSync('node server/db/init.js', { 
+            studio: 'inherit',
+            cwd: path.resolve(__dirname, '../')
+    });
+    console.log('Base de données initialisée avec succès.');
+    } catch (error) {
+        console.error('Erreur lors de l\'initialisation de la base de données :', error);
+        process.exit(1);
+    }
+} else {
     console.log('Base de données trouvée, démarrage du serveur...');
 }
 
